1e6dbf54c064ec1d0c92d7b5a29b515e11785de6
QuantumlyTangled <ethoarx@gmail.com>
2021-03-22 07:32:56 +0100

    refactor: comment out company display

--- src/pages/Where.tsx
+++ src/pages/Where.tsx
@@ -1,23 +1,17 @@
+import Repo, { Language } from "../components/Repo";
 import PageWrapper from "./PageWrapper";
+/*
 import Co from "../components/Co";
 import styled from "styled-components";
 import HivenAppIcon from '../assets/images/hiven-app-icon.png';
 import GigglAppIcon from '../assets/images/giggl-app-icon.jpg';
 import HonkAppIcon from '../assets/images/honk-app-icon.jpg';
-import Repo, { Language } from "../components/Repo";
+*/
 
 const Where = () => {
   return (
     <PageWrapper>
-       <h1>Where I've Done It</h1>
-       <h3>Companies</h3>
-      <CoWrapper>
-        <Co url="https://hiven.io" name="Hiven" iconReference={HivenAppIcon} tagline="Consumer social" role={"Founder & Developer"} what={"I founded Hiven back in 2019 to make it easy for anyone create premium groups."}/>
-        <Co url="https://giggl.app" name="Giggl" iconReference={GigglAppIcon} tagline="Consumer social" role={"Co-founder & Developer"} what={"I helped design and create the backend, realtime infrastructure and the frontend."}/>
-        <Co url="https://honk.me" name="Honk" iconReference={HonkAppIcon} tagline="Consumer social" role={"Backend Engineer"} what={"I designed and implemented the realtime infrastructure at Honk using Elixir."}/>
-
-      </CoWrapper>
-
+      <h1>Where I've Done It</h1>
       <h3>Open-source Projects</h3>
       <Repo name={"lanyard"} url={"https://github.com/phineas/lanyard"} primaryLanguage={Language.Elixir} description="Expose your Discord presence to an API in <10 seconds (used on this site)"/>
       <Repo name={"node_compass"} url={"https://github.com/hivenapp/node_compass"} primaryLanguage={Language.Elixir} description="Automatic hash ring management for Elixir nodes"/>
@@ -25,6 +19,15 @@ const Where = () => {
     </PageWrapper>
   )
 }
+
+/*
+
+<h3>Companies</h3>
+<CoWrapper>  
+  <Co url="https://hiven.io" name="Hiven" iconReference={HivenAppIcon} tagline="Consumer social" role={"Founder & Developer"} what={"I founded Hiven back in 2019 to make it easy for anyone create premium groups."}/>
+  <Co url="https://giggl.app" name="Giggl" iconReference={GigglAppIcon} tagline="Consumer social" role={"Co-founder & Developer"} what={"I helped design and create the backend, realtime infrastructure and the frontend."}/>
+  <Co url="https://honk.me" name="Honk" iconReference={HonkAppIcon} tagline="Consumer social" role={"Backend Engineer"} what={"I designed and implemented the realtime infrastructure at Honk using Elixir."}/>
+</CoWrapper>
 
 const CoWrapper = styled.div`
   display: grid;
@@ -40,5 +43,7 @@ const CoWrapper = styled.div`
     grid-template-columns: 1fr;
   }
 `;
+
+*/
 
 export default Where;
