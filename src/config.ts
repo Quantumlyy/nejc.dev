f75f190e8d668fe4b81ab1838620123b9aab8edd
QuantumlyTangled <ethoarx@gmail.com>
2021-03-27 06:33:58 +0100

    refactor: add skyra to oss projects

--- src/config.ts
+++ src/config.ts
@@ -17,5 +17,11 @@ export const config: Config = {
     discordId: '126321762483830785',
     twitterId: '3044781411',
     repos: [
+        {
+            name: 'Skyra',
+            url: 'https://github.com/skyra-project/skyra',
+            lang: Language.TypeScript,
+            description: 'All-in-one multipurpose Discord Bot designed to carry out most of your server\'s needs with great performance and stability.'
+        }
     ]
 };
