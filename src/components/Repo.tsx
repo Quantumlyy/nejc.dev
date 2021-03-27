22c3156ac491d1a319f07639bfba7980d244acab
QuantumlyTangled <ethoarx@gmail.com>
2021-03-27 09:15:39 +0100

    refactor: adjust border coloring

--- src/components/Repo.tsx
+++ src/components/Repo.tsx
@@ -66,7 +66,7 @@ const Container = styled.div`
   flex-direction: row;
   width: 100%;
   height: 100px;
-  border: 1px solid #101010;
+  border: 1px solid white;
   border-radius: 10px;
   overflow: hidden;
   margin-bottom: 1rem;
