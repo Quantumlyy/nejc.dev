489c73b45bca9c7abc157a2926371dd88605416f
QuantumlyTangled <ethoarx@gmail.com>
2021-04-09 07:42:01 +0200

    feat: initial translations

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './i18n';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
