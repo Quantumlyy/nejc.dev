b76a54fe436a079030868f3b744794c24dea15f9
QuantumlyTangled <ethoarx@gmail.com>
2021-05-02 08:22:01 +0200

    refactor: remove redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,17 @@
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import React from 'react';
-import { Provider as ReduxProvider } from 'react-redux';
-import { store } from 'state';
-import LanyardTrack from 'components/LanyardTrack';
 import 'styles/_App.css';
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
 	return (
-		<ReduxProvider store={store}>
-			<LanyardTrack>
-				<div className="wrapper">
-					<div className="contentMain">
-						<div className="contentWrapper">
-							<Component {...pageProps} />
-						</div>
-					</div>
+		<div className="wrapper">
+			<div className="contentMain">
+				<div className="contentWrapper">
+					<Component {...pageProps} />
 				</div>
-			</LanyardTrack>
-		</ReduxProvider>
+			</div>
+		</div>
 	);
 };
 
