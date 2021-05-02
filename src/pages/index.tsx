09a8836e00db32cf49bdedced8b10d7f4a9a3a71
QuantumlyTangled <ethoarx@gmail.com>
2021-05-02 07:57:01 +0200

    feat: index page

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next';
+import React from 'react';
+import PageWrapper from '../components/PageWrapper';
+
+const What: NextPage = () => {
+	return (
+		<PageWrapper forceReadableWidth>
+			<h1>What I Do</h1>
+			<p>17 y/o developer, designer, and innovator.</p>
+		</PageWrapper>
+	);
+};
+
+export default What;
