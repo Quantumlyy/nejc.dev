ae390fb9e752c9242d2adaaf66c8efe2f2c0ccc1
QuantumlyTangled <ethoarx@gmail.com>
2021-05-03 22:36:37 +0200

    fix: proper background size

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Layout: React.FC = (props) => {
-	return <div className="flex h-screen flex-col p-5 md:p-14">{props.children}</div>;
+	return <div className="flex min-h-screen flex-col p-5 md:p-14">{props.children}</div>;
 };
 
 export default Layout;
