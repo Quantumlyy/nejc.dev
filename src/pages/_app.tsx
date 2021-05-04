2e305d29a3127990eeba9b0cdbdaf8ec0ee76219
QuantumlyTangled <ethoarx@gmail.com>
2021-05-04 07:15:22 +0200

    feat: better SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
+import { animations } from 'core/animations';
+import { DefaultSEOProps } from 'DefaultSEOProps';
 import { AnimatePresence, motion } from 'framer-motion';
 import type { NextPage } from 'next';
+import { DefaultSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { Router } from 'next/router';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
 import React, { StrictMode, useEffect } from 'react';
-import { animations } from 'core/animations';
 import 'styles/_App.css';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
@@ -22,8 +24,14 @@ const App: NextPage<AppProps> = ({ Component, pageProps, router }) => {
 	return (
 		<StrictMode>
 			<Head>
-				<title>Nejc Drobnič</title>
+				<meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
+				<meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+				<meta httpEquiv="Expires" content="1y" />
+				<meta httpEquiv="Pragma" content="1y" />
+				<meta httpEquiv="Cache-Control" content="1y" />
 			</Head>
+			<DefaultSeo {...DefaultSEOProps} />
+
 			<div className="h-full bg-blur">
 				<AnimatePresence exitBeforeEnter>
 					<motion.div key={router.pathname} {...animations} className="h-full">
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,9 +6,6 @@ class MyDocument extends Document {
 		return (
 			<Html lang="en" prefix="og: https://ogp.me/ns#">
 				<Head>
-					<meta charSet="utf-8" />
-					<meta name="theme-color" content="#ffe8e8" />
-					<meta name="description" content="Nejc Drobnič, Full–stack TypeScript engineer from SI" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link
 						href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Space+Mono:wght@700&display=swap"
