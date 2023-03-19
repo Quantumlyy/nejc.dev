a975a79990c587e6954b4de7f8b39fca76768de7
Quantumlyy <ethoarx@gmail.com>
2023-03-19 22:25:46 +0100

    update deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,80 +1,57 @@
+// @ts-check
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+const {default: flattenColorPalette} = require('tailwindcss/lib/util/flattenColorPalette');
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-	purge: ['./src/**/*.{tsx,ts}'],
-	darkMode: 'class',
+	content: ['./{src,app}/**/*.{ts,tsx}'],
 	theme: {
+		fontFamily: {
+			sans: ['var(--font-inter)', ...defaultTheme.fontFamily.sans],
+			title: ['var(--font-title)', ...defaultTheme.fontFamily.serif],
+		},
 		extend: {
-			typography: (theme) => ({
-				default: {
-					css: {
-						color: theme('colors.gray.900'),
-						a: {
-							color: theme('colors.blue.700'),
-							'&:hover': {
-								color: theme('colors.blue.500')
-							}
-						}
-					}
-				},
-				dark: {
-					css: {
-						color: theme('colors.gray.300'),
-						a: {
-							color: theme('colors.purple.500'),
-							'&:hover': {
-								color: theme('colors.purple.400')
-							}
-						},
-
-						h1: {
-							color: theme('colors.gray.100')
-						},
-						h2: {
-							color: theme('colors.gray.100')
-						},
-						h3: {
-							color: theme('colors.gray.100')
-						},
-						h4: {
-							color: theme('colors.gray.100')
-						},
-						h5: {
-							color: theme('colors.gray.100')
-						},
-						h6: {
-							color: theme('colors.gray.100')
-						},
-
-						strong: {
-							color: theme('colors.gray.100')
-						},
-
-						code: {
-							color: theme('colors.gray.300')
-						},
-
-						figcaption: {
-							color: theme('colors.gray.500')
-						}
-					}
-				}
-			}),
-			scale: {
-				102.5: '1.025'
-			},
 			colors: {
-				mix: {
-					shadow: 'linear-gradient(156deg,#000 30%,#007acc 59%,#9c1fa5 72%,#9c1fa5 89%)'
-				},
-				slightlyNotWhite: '#ededed',
-				base: '#0f0f0f'
+				blurple: '#5865F2',
 			},
-			screens: {
-				xs: '481px'
-			}
-		}
+		},
 	},
-	variants: {
-		typography: ['dark']
-	},
-	plugins: [require('@tailwindcss/typography')]
+	plugins: [
+		require('@tailwindcss/forms'),
+		{
+			handler: tw => {
+				tw.matchComponents(
+					{
+						'bg-grid': value => ({
+							backgroundSize: '90px 90px',
+							backgroundImage: `
+								linear-gradient(to right, ${value} 1px, transparent 1px),
+								linear-gradient(to bottom, ${value} 1px, transparent 1px)
+							`,
+						}),
+					},
+					{
+						values: flattenColorPalette(tw.theme('colors')),
+						type: 'color',
+					},
+				);
+
+				tw.matchUtilities(
+					{
+						'text-glow': value => ({
+							'text-shadow': `0 0 10px ${value}, 0 0 150px ${value}`,
+						}),
+						'glow': value => ({
+							filter: `drop-shadow(0px 0px 7px ${value})`,
+						}),
+					},
+					{
+						values: flattenColorPalette(tw.theme('colors')),
+						type: 'color',
+					},
+				);
+			},
+		},
+	],
 };
